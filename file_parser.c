1d44f943c4530a9d3ee916194959414de4541040
hvrcharon1 <harshalrasal792@gmail.com>
2019-11-22 19:30:00 -0500

    My Machine Upload
    Edited file_parser.c and lsq_driver.c

--- file_parser.c
+++ file_parser.c
@@ -16,17 +16,17 @@
  *
  * Note : You are not supposed to edit this function
  */
-static int
-get_num_from_string(char* buffer)
+static int get_num_from_string(char* buffer)
 {
-  char str[16];
-  int j = 0;
-  for (int i = 1; buffer[i] != '\0'; ++i) {
-    str[j] = buffer[i];
-    j++;
-  }
-  str[j] = '\0';
-  return atoi(str);
+    char str[16];
+    int j = 0;
+    for (int i = 1; buffer[i] != '\0'; ++i)
+    {
+        str[j] = buffer[i];
+        j++;
+    }
+    str[j] = '\0';
+    return atoi(str);
 }
 
 /*
@@ -34,104 +34,107 @@ get_num_from_string(char* buffer)
  *
  * Note : you can edit this function to add new instructions
  */
-static void
-create_APEX_instruction(APEX_Instruction* ins, char* buffer)
+static void create_APEX_instruction(APEX_Instruction* ins, char* buffer)
 {
-  char* token = strtok(buffer, ",");
-  int token_num = 0;
-  char tokens[6][128];
-  while (token != NULL) {
-    strcpy(tokens[token_num], token);
-    token_num++;
-    token = strtok(NULL, ",");
-  }
-
-  strcpy(ins->opcode, tokens[0]);
-
-  if (strcmp(ins->opcode, "MOVC") == 0) {
-    ins->rd = get_num_from_string(tokens[1]);
-
-    if (ins->rd > 15 || ins->rd < 0) {
-      exception_handler(1, ins->opcode);
+    char* token = strtok(buffer, ",");
+    int token_num = 0;
+    char tokens[6][128];
+    while (token != NULL) 
+    {
+        strcpy(tokens[token_num], token);
+        token_num++;
+        token = strtok(NULL, ",");
     }
 
-    ins->imm = get_num_from_string(tokens[2]);
-  }
+    strcpy(ins->opcode, tokens[0]);
 
-  if (strcmp(ins->opcode, "STORE") == 0) {
-    ins->rs1 = get_num_from_string(tokens[1]);
-    ins->rs2 = get_num_from_string(tokens[2]);
+    if (strcmp(ins->opcode, "MOVC") == 0) 
+    {
+        ins->rd = get_num_from_string(tokens[1]);
 
-   if ((ins->rs1 > 15 || ins->rs1 < 0) ||
-       (ins->rs2 > 15 || ins->rs2 < 0)) {
-      exception_handler(1, ins->opcode);
-   }
+            if (ins->rd > 15 || ins->rd < 0) 
+            {
+                exception_handler(1, ins->opcode);
+            }
 
-   ins->imm = get_num_from_string(tokens[3]);
-  }
-
-  if (strcmp(ins->opcode, "LOAD") == 0) {
-    ins->rd = get_num_from_string(tokens[1]);
-    ins->rs1 = get_num_from_string(tokens[2]);
-    ins->imm = get_num_from_string(tokens[3]);
-  }
-
-  if (strcmp(ins->opcode, "ADD") == 0 ||
-      strcmp(ins->opcode, "SUB") == 0 ||
-      strcmp(ins->opcode, "AND") == 0 ||
-      strcmp(ins->opcode, "OR") == 0 ||
-      strcmp(ins->opcode, "EX-OR") == 0 ||
-      strcmp(ins->opcode, "MUL") == 0) {
-
-    ins->rd = get_num_from_string(tokens[1]);
-    ins->rs1 = get_num_from_string(tokens[2]);
-    ins->rs2 = get_num_from_string(tokens[3]);
-
-    if ((ins->rs1 > 15 || ins->rs1 < 0) ||
-        (ins->rs2 > 15 || ins->rs2 < 0) ||
-        (ins->rd > 15 || ins->rd < 0)) {
-      exception_handler(1, ins->opcode);
-    }
-  }
-
-  if (strcmp(ins->opcode, "ADDL") == 0 ||
-      strcmp(ins->opcode, "SUBL") == 0)  {
-
-    ins->rd = get_num_from_string(tokens[1]);
-    ins->rs1 = get_num_from_string(tokens[2]);
-    ins->imm = get_num_from_string(tokens[3]);
-
-    if ((ins->rs1 > 15 || ins->rs1 < 0) ||
-        (ins->rd > 15 || ins->rd < 0)) {
-      exception_handler(1, ins->opcode);
-    }
-  }
-
-  if (strcmp(ins->opcode, "BZ") == 0 ||
-      strcmp(ins->opcode, "BNZ") == 0) {
-    ins->imm = get_num_from_string(tokens[1]);
-  }
-
-  if (strcmp(ins->opcode, "JUMP") == 0) {
-    ins->rs1 = get_num_from_string(tokens[1]);
-
-    if (ins->rs1 > 15 || ins->rs1 < 0) {
-      exception_handler(1, ins->opcode);
+        ins->imm = get_num_from_string(tokens[2]);
     }
 
-    ins->imm = get_num_from_string(tokens[2]);
-  }
+    if (strcmp(ins->opcode, "STORE") == 0) 
+    {
+        ins->rs1 = get_num_from_string(tokens[1]);
+        ins->rs2 = get_num_from_string(tokens[2]);
 
-  if (strcmp(ins->opcode, "JAL") == 0) {
-    ins->rd = get_num_from_string(tokens[1]);
-    ins->rs1 = get_num_from_string(tokens[2]);
+            if ((ins->rs1 > 15 || ins->rs1 < 0) || (ins->rs2 > 15 || ins->rs2 < 0)) 
+            {
+                exception_handler(1, ins->opcode);
+            }
 
-    if (ins->rs1 > 15 || ins->rs1 < 0) {
-      exception_handler(1, ins->opcode);
+        ins->imm = get_num_from_string(tokens[3]);
     }
 
-    ins->imm = get_num_from_string(tokens[3]);
-  }
+    if (strcmp(ins->opcode, "LOAD") == 0) 
+    {
+        ins->rd = get_num_from_string(tokens[1]);
+        ins->rs1 = get_num_from_string(tokens[2]);
+        ins->imm = get_num_from_string(tokens[3]);
+    }
+
+    if (strcmp(ins->opcode, "ADD") == 0 || strcmp(ins->opcode, "SUB") == 0 || strcmp(ins->opcode, "AND") == 0 || strcmp(ins->opcode, "OR") == 0 || strcmp(ins->opcode, "EX-OR") == 0 || strcmp(ins->opcode, "MUL") == 0) 
+    {
+
+        ins->rd = get_num_from_string(tokens[1]);
+        ins->rs1 = get_num_from_string(tokens[2]);
+        ins->rs2 = get_num_from_string(tokens[3]);
+
+            if ((ins->rs1 > 15 || ins->rs1 < 0) || (ins->rs2 > 15 || ins->rs2 < 0) || (ins->rd > 15 || ins->rd < 0)) 
+            {
+                exception_handler(1, ins->opcode);
+            }
+    }
+
+    if (strcmp(ins->opcode, "ADDL") == 0 || strcmp(ins->opcode, "SUBL") == 0)  
+    {
+
+        ins->rd = get_num_from_string(tokens[1]);
+        ins->rs1 = get_num_from_string(tokens[2]);
+        ins->imm = get_num_from_string(tokens[3]);
+
+            if ((ins->rs1 > 15 || ins->rs1 < 0) || (ins->rd > 15 || ins->rd < 0)) 
+            {
+                exception_handler(1, ins->opcode);
+            }
+    }
+
+    if (strcmp(ins->opcode, "BZ") == 0 || strcmp(ins->opcode, "BNZ") == 0) 
+    {
+        ins->imm = get_num_from_string(tokens[1]);
+    }
+
+    if (strcmp(ins->opcode, "JUMP") == 0) 
+    {
+        ins->rs1 = get_num_from_string(tokens[1]);
+
+            if (ins->rs1 > 15 || ins->rs1 < 0) 
+            {
+                exception_handler(1, ins->opcode);
+            }
+
+        ins->imm = get_num_from_string(tokens[2]);
+    }
+
+    if (strcmp(ins->opcode, "JAL") == 0) 
+    {
+        ins->rd = get_num_from_string(tokens[1]);
+        ins->rs1 = get_num_from_string(tokens[2]);
+
+            if (ins->rs1 > 15 || ins->rs1 < 0) 
+            {
+                exception_handler(1, ins->opcode);
+            }
+
+        ins->imm = get_num_from_string(tokens[3]);
+    }
 }
 
 /*
@@ -139,47 +142,54 @@ create_APEX_instruction(APEX_Instruction* ins, char* buffer)
  *
  * Note : You are not supposed to edit this function
  */
-APEX_Instruction*
-create_code_memory(const char* filename, int* size)
+APEX_Instruction* create_code_memory(const char* filename, int* size)
 {
-  if (!filename) {
-    return NULL;
-  }
+    if (!filename) 
+    {
+        return NULL;
+    }
 
-  FILE* fp = fopen(filename, "r");
-  if (!fp) {
-    return NULL;
-  }
+    FILE* fp = fopen(filename, "r");
+    if (!fp) 
+    {
+        return NULL;
+    }
 
-  char* line = NULL;
-  size_t len = 0;
-  ssize_t nread;
-  int code_memory_size = 0;
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    int code_memory_size = 0;
 
-  while ((nread = getline(&line, &len, fp)) != -1) {
-    code_memory_size++;
-  }
-  *size = code_memory_size;
-  if (!code_memory_size) {
+    while ((nread = getline(&line, &len, fp)) != -1) 
+    {
+        code_memory_size++;
+    }
+    
+    *size = code_memory_size;
+    
+    if (!code_memory_size) 
+    {
+        fclose(fp);
+        return NULL;
+    }
+
+    APEX_Instruction* code_memory = malloc(sizeof(*code_memory) * code_memory_size);
+    
+    if (!code_memory) 
+    {
+        fclose(fp);
+        return NULL;
+    }
+
+    rewind(fp);
+    int current_instruction = 0;
+    while ((nread = getline(&line, &len, fp)) != -1) 
+    {
+        create_APEX_instruction(&code_memory[current_instruction], line);
+        current_instruction++;
+    }
+
+    free(line);
     fclose(fp);
-    return NULL;
-  }
-
-  APEX_Instruction* code_memory =
-    malloc(sizeof(*code_memory) * code_memory_size);
-  if (!code_memory) {
-    fclose(fp);
-    return NULL;
-  }
-
-  rewind(fp);
-  int current_instruction = 0;
-  while ((nread = getline(&line, &len, fp)) != -1) {
-    create_APEX_instruction(&code_memory[current_instruction], line);
-    current_instruction++;
-  }
-
-  free(line);
-  fclose(fp);
-  return code_memory;
+    return code_memory;
 }
--- lsq_driver.c
+++ lsq_driver.c
@@ -14,17 +14,16 @@
 #include "rob_driver.h"
 
 
-int
-is_lsq_entry_free(APEX_CPU* cpu)
+int is_lsq_entry_free(APEX_CPU* cpu)
 {
-  if (cpu->lsq.lsq_entry[cpu->lsq.tail].free) {
-    return 1;
-  }
-  return 0;
+    if (cpu->lsq.lsq_entry[cpu->lsq.tail].free) 
+    {
+        return 1;
+    }
+    return 0;
 }
 
-int
-push_lsq_entry(APEX_CPU* cpu, LSQ_Entry* new_lsq_entry)
+int push_lsq_entry(APEX_CPU* cpu, LSQ_Entry* new_lsq_entry)
 {
   int free_entry = cpu->lsq.tail;
   cpu->lsq.lsq_entry[free_entry].free = new_lsq_entry->free;
@@ -45,37 +44,40 @@ push_lsq_entry(APEX_CPU* cpu, LSQ_Entry* new_lsq_entry)
   cpu->lsq.lsq_entry[free_entry].phys_rd = new_lsq_entry->phys_rd;
 
   cpu->lsq.tail++;
-  if (cpu->lsq.tail == LSQ_ENTRIES_NUMBER) {
-    cpu->lsq.tail = 0;
+  if (cpu->lsq.tail == LSQ_ENTRIES_NUMBER) 
+  {
+      cpu->lsq.tail = 0;
   }
   return free_entry;
 }
 
-void
-get_instruction_to_MEM(APEX_CPU* cpu)
+void get_instruction_to_MEM(APEX_CPU* cpu)
 {
-  int push_to_mem = 0;
-  int entry = cpu->lsq.head;
-  if (!cpu->lsq.lsq_entry[entry].free &&
-      cpu->lsq.lsq_entry[entry].mem_address_valid &&
-      !cpu->stage[MEM].stalled) {
-
-    if (strcmp(cpu->lsq.lsq_entry[entry].opcode, "STORE") == 0) {
-      if (cpu->lsq.lsq_entry[entry].rs1_ready && cpu->commitments != 2) {
-        int rob_head = cpu->rob.head;
-        if (strcmp(cpu->rob.rob_entry[rob_head].opcode, "STORE") == 0) {
-          push_to_mem = 1;
-          // delete from ROB
-          remove_store_from_rob(cpu);
+    int push_to_mem = 0;
+    int entry = cpu->lsq.head;
+    if (!cpu->lsq.lsq_entry[entry].free && cpu->lsq.lsq_entry[entry].mem_address_valid && !cpu->stage[MEM].stalled) 
+    {
+        if (strcmp(cpu->lsq.lsq_entry[entry].opcode, "STORE") == 0) 
+        {
+            if (cpu->lsq.lsq_entry[entry].rs1_ready && cpu->commitments != 2) 
+            {
+                int rob_head = cpu->rob.head;
+                if (strcmp(cpu->rob.rob_entry[rob_head].opcode, "STORE") == 0) 
+                {
+                    push_to_mem = 1;
+                    // delete from ROB
+                    remove_store_from_rob(cpu);
+                }
+            }
         }
+    else 
+      {
+        push_to_mem = 1;
       }
     }
-    else {
-      push_to_mem = 1;
-    }
-  }
 
-  if (push_to_mem) {
+  if (push_to_mem) 
+  {
     cpu->stage[MEM].pc = cpu->lsq.lsq_entry[entry].pc;
     strcpy(cpu->stage[MEM].opcode, cpu->lsq.lsq_entry[entry].opcode);
     cpu->stage[MEM].arch_rd = cpu->lsq.lsq_entry[entry].arch_rd;
@@ -94,158 +96,179 @@ get_instruction_to_MEM(APEX_CPU* cpu)
 
     cpu->lsq.lsq_entry[entry].free = 1;
     cpu->lsq.head++;
-    if (cpu->lsq.head == LSQ_ENTRIES_NUMBER) {
-      cpu->lsq.head = 0;
+        if (cpu->lsq.head == LSQ_ENTRIES_NUMBER) 
+        {
+            cpu->lsq.head = 0;
+        }
     }
-  }
 }
 
-void
-update_lsq_entry(APEX_CPU* cpu, enum STAGES FU_type)
+void update_lsq_entry(APEX_CPU* cpu, enum STAGES FU_type)
 {
-  int LSQ_index = cpu->stage[FU_type].LSQ_index;
-  cpu->lsq.lsq_entry[LSQ_index].mem_address = cpu->stage[FU_type].buffer;
-  cpu->lsq.lsq_entry[LSQ_index].mem_address_valid = 1;
+    int LSQ_index = cpu->stage[FU_type].LSQ_index;
+    cpu->lsq.lsq_entry[LSQ_index].mem_address = cpu->stage[FU_type].buffer;
+    cpu->lsq.lsq_entry[LSQ_index].mem_address_valid = 1;
 }
 
-void
-broadcast_result_into_lsq(APEX_CPU* cpu, enum STAGES FU_type)
+void broadcast_result_into_lsq(APEX_CPU* cpu, enum STAGES FU_type)
 {
-  for (int i = 0; i < LSQ_ENTRIES_NUMBER; i++) {
-    if (!cpu->lsq.lsq_entry[i].free &&
-        cpu->lsq.lsq_entry[i].phys_rs1 == cpu->stage[FU_type].phys_rd) {
-      cpu->lsq.lsq_entry[i].rs1_value = cpu->stage[FU_type].buffer;
-      cpu->lsq.lsq_entry[i].rs1_ready = 1;
+    for (int i = 0; i < LSQ_ENTRIES_NUMBER; i++) 
+    {
+        if (!cpu->lsq.lsq_entry[i].free &&
+        cpu->lsq.lsq_entry[i].phys_rs1 == cpu->stage[FU_type].phys_rd) 
+        {
+            cpu->lsq.lsq_entry[i].rs1_value = cpu->stage[FU_type].buffer;
+            cpu->lsq.lsq_entry[i].rs1_ready = 1;
+        }
     }
-  }
 }
 
-void
-print_lsq_for_debug(APEX_CPU* cpu)
+void print_lsq_for_debug(APEX_CPU* cpu)
 {
-  printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-  printf("Details of LSQ State\n");
-  for (int i = 0; i < LSQ_ENTRIES_NUMBER; i++) {
-    if (!cpu->lsq.lsq_entry[i].free) {
-      printf("| ID=%d, OPCODE=%s, PC=%d, MAV=%d, MA=%d, BR=%d, ROB=%d, RS1_READY=%d, PHYS_RS1=%d, RS1_VALUE=%d, PHYS_RS2=%d, IMM=%d, ARCH_RD=%d PHYS_RD=%d |\n",
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("Details of LSQ State\n");
+    for (int i = 0; i < LSQ_ENTRIES_NUMBER; i++) 
+    {
+        if (!cpu->lsq.lsq_entry[i].free) 
+        {
+            printf("| ID=%d, OPCODE=%s, PC=%d, MAV=%d, MA=%d, BR=%d, ROB=%d, RS1_READY=%d, PHYS_RS1=%d, RS1_VALUE=%d, PHYS_RS2=%d, IMM=%d, ARCH_RD=%d PHYS_RD=%d |\n",
               i, cpu->lsq.lsq_entry[i].opcode, cpu->lsq.lsq_entry[i].pc,
               cpu->lsq.lsq_entry[i].mem_address_valid, cpu->lsq.lsq_entry[i].mem_address,
               cpu->lsq.lsq_entry[i].branch_id, cpu->lsq.lsq_entry[i].rob_entry_id,
               cpu->lsq.lsq_entry[i].rs1_ready, cpu->lsq.lsq_entry[i].phys_rs1, cpu->lsq.lsq_entry[i].rs1_value,
               cpu->lsq.lsq_entry[i].phys_rs2, cpu->lsq.lsq_entry[i].imm, cpu->lsq.lsq_entry[i].arch_rd,
               cpu->lsq.lsq_entry[i].phys_rd);
+        }
     }
-  }
-  printf("Tail: %d, Head: %d\n\n", cpu->lsq.tail, cpu->lsq.head);
+    printf("Tail: %d, Head: %d\n\n", cpu->lsq.tail, cpu->lsq.head);
 }
 
-void
-display_lsq(APEX_CPU* cpu)
+void display_lsq(APEX_CPU* cpu)
 {
-  printf("------------------------------- Load Store Queue --------------------------------\n");
-  for (int i = 0; i < LSQ_ENTRIES_NUMBER; i++) {
-    if (!cpu->lsq.lsq_entry[i].free || i == cpu->lsq.tail) {
+    printf("------------------------------- Load Store Queue --------------------------------\n");
+    for (int i = 0; i < LSQ_ENTRIES_NUMBER; i++) 
+    {
+        if (!cpu->lsq.lsq_entry[i].free || i == cpu->lsq.tail) 
+        {
+            printf("| Index = %d | ", i);
 
-      printf("| Index = %d | ", i);
+                if (i == cpu->lsq.tail) 
+                {
+                    printf("t |");
+                }
+                else 
+                {
+                    printf("  |");
+                }
 
-      if (i == cpu->lsq.tail) {
-        printf("t |");
-      }
-      else {
-        printf("  |");
-      }
+                    if (i == cpu->lsq.head) 
+                    {
+                        printf(" h |\t");
+                    }
+                    else 
+                    {
+                        printf("   |\t");
+                    }
 
-      if (i == cpu->lsq.head) {
-        printf(" h |\t");
-      }
-      else {
-        printf("   |\t");
-      }
-
-      //printf("\t");
-      if (!cpu->lsq.lsq_entry[i].free) {
-      printf("pc(%d)  ", cpu->lsq.lsq_entry[i].pc);
-        CPU_Stage* instruction_to_print = malloc(sizeof(*instruction_to_print));
-        strcpy(instruction_to_print->opcode, cpu->lsq.lsq_entry[i].opcode);
-        instruction_to_print->arch_rs1 = cpu->lsq.lsq_entry[i].arch_rs1;
-        instruction_to_print->phys_rs1 = cpu->lsq.lsq_entry[i].phys_rs1;
-        instruction_to_print->arch_rs2 = cpu->lsq.lsq_entry[i].arch_rs2;
-        instruction_to_print->phys_rs2 = cpu->lsq.lsq_entry[i].phys_rs2;
-        instruction_to_print->arch_rd = cpu->lsq.lsq_entry[i].arch_rd;
-        instruction_to_print->phys_rd = cpu->lsq.lsq_entry[i].phys_rd;
-        instruction_to_print->imm = cpu->lsq.lsq_entry[i].imm;
-        print_instruction(0, instruction_to_print);
-        printf("\t|");
-      }
-      printf("\n");
+                        //printf("\t");
+                        if (!cpu->lsq.lsq_entry[i].free) 
+                        {
+                            printf("pc(%d)  ", cpu->lsq.lsq_entry[i].pc);
+                            CPU_Stage* instruction_to_print = malloc(sizeof(*instruction_to_print));
+                            strcpy(instruction_to_print->opcode, cpu->lsq.lsq_entry[i].opcode);
+                            instruction_to_print->arch_rs1 = cpu->lsq.lsq_entry[i].arch_rs1;
+                            instruction_to_print->phys_rs1 = cpu->lsq.lsq_entry[i].phys_rs1;
+                            instruction_to_print->arch_rs2 = cpu->lsq.lsq_entry[i].arch_rs2;
+                            instruction_to_print->phys_rs2 = cpu->lsq.lsq_entry[i].phys_rs2;
+                            instruction_to_print->arch_rd = cpu->lsq.lsq_entry[i].arch_rd;
+                            instruction_to_print->phys_rd = cpu->lsq.lsq_entry[i].phys_rd;
+                            instruction_to_print->imm = cpu->lsq.lsq_entry[i].imm;
+                            print_instruction(0, instruction_to_print);
+                            printf("\t|");
+                        }
+                        printf("\n");
+        }
     }
-  }
-  printf("---------------------------------------------------------------------------------\n\n");
+    printf("---------------------------------------------------------------------------------\n\n");
 }
 
-int
-is_lsq_empty(APEX_CPU* cpu)
+int is_lsq_empty(APEX_CPU* cpu)
 {
-  for (int i = 0; i < LSQ_ENTRIES_NUMBER; i++) {
-    if (!cpu->lsq.lsq_entry[i].free){
-      return 0;
+    for (int i = 0; i < LSQ_ENTRIES_NUMBER; i++) 
+    {
+        if (!cpu->lsq.lsq_entry[i].free)
+        {
+            return 0;
+        }
     }
-  }
-  return 1;
+    return 1;
 }
 
-void
-flush_lsq(APEX_CPU* cpu, int branch_id)
+void flush_lsq(APEX_CPU* cpu, int branch_id)
 {
-  if (branch_id <= cpu->last_branch_id) {
-    while (branch_id <= cpu->last_branch_id) {
-      for (int i = 0; i < LSQ_ENTRIES_NUMBER; i++) {
-        if (!cpu->lsq.lsq_entry[i].free && cpu->lsq.lsq_entry[i].branch_id == branch_id) {
-          cpu->lsq.lsq_entry[i].free = 1;
+    if (branch_id <= cpu->last_branch_id) 
+    {
+        while (branch_id <= cpu->last_branch_id) 
+        {
+            for (int i = 0; i < LSQ_ENTRIES_NUMBER; i++) 
+            {
+                if (!cpu->lsq.lsq_entry[i].free && cpu->lsq.lsq_entry[i].branch_id == branch_id) 
+                {
+                    cpu->lsq.lsq_entry[i].free = 1;
+                }
+            }
+        branch_id++;
         }
-      }
-      branch_id++;
     }
-  }
-  else {
-    while (branch_id < BIS_ENTRIES_NUMBER) {
-      for (int i = 0; i < LSQ_ENTRIES_NUMBER; i++) {
-        if (!cpu->lsq.lsq_entry[i].free && cpu->lsq.lsq_entry[i].branch_id == branch_id) {
-          cpu->lsq.lsq_entry[i].free = 1;
+    else 
+    {
+        while (branch_id < BIS_ENTRIES_NUMBER) 
+        {
+            for (int i = 0; i < LSQ_ENTRIES_NUMBER; i++) 
+            {
+                if (!cpu->lsq.lsq_entry[i].free && cpu->lsq.lsq_entry[i].branch_id == branch_id) 
+                {
+                    cpu->lsq.lsq_entry[i].free = 1;
+                }
+            }
+            branch_id++;
         }
-      }
-      branch_id++;
-    }
-    branch_id = 0;
-    while (branch_id <= cpu->last_branch_id) {
-      for (int i = 0; i < LSQ_ENTRIES_NUMBER; i++) {
-        if (!cpu->lsq.lsq_entry[i].free && cpu->lsq.lsq_entry[i].branch_id == branch_id) {
-          cpu->lsq.lsq_entry[i].free = 1;
+        branch_id = 0;
+        while (branch_id <= cpu->last_branch_id) 
+        {
+            for (int i = 0; i < LSQ_ENTRIES_NUMBER; i++) 
+            {
+                if (!cpu->lsq.lsq_entry[i].free && cpu->lsq.lsq_entry[i].branch_id == branch_id) 
+                {
+                    cpu->lsq.lsq_entry[i].free = 1;
+                }
+            }
+            branch_id++;
         }
-      }
-      branch_id++;
     }
-  }
 
-  if (is_lsq_empty(cpu)) {
-    cpu->lsq.tail = 0;
-    cpu->lsq.head = 0;
-  }
-  else {
+    if (is_lsq_empty(cpu)) 
+    {
+        cpu->lsq.tail = 0;
+        cpu->lsq.head = 0;
+    }
+    else 
+    {
     //int previous_branch_id = initial_branch_id--;
-    int new_tail = cpu->lsq.head;
-    while (!cpu->lsq.lsq_entry[new_tail].free) {
-      new_tail++;
-      if (new_tail == LSQ_ENTRIES_NUMBER) {
-        new_tail = 0;
-      }
+        int new_tail = cpu->lsq.head;
+        while (!cpu->lsq.lsq_entry[new_tail].free) 
+        {
+            new_tail++;
+                if (new_tail == LSQ_ENTRIES_NUMBER) 
+                {
+                    new_tail = 0;
+                }
+        }
+        cpu->lsq.tail = new_tail;
     }
-    cpu->lsq.tail = new_tail;
-  }
 }
 
-void
-process_lsq(APEX_CPU* cpu)
+void process_lsq(APEX_CPU* cpu)
 {
-  get_instruction_to_MEM(cpu);
+    get_instruction_to_MEM(cpu);
 }
